fe5fe3665065498b85184ae266e0a426722f13c0
Robert Graupner <robertgraupner@air-robert.home>
2017-07-09 16:30:23 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api',
+    myHeaders = {
+        'X-Client-Id': '1923',
+        'X-Auth-Token': '0596f472213b022761020ff05fb541fd'
+    };
+$.ajaxSetup({
+    headers: myHeaders
+});
+function setupColumn(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.createCard(card);
+    });
+}
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+        setupColumn(response.columns);
+    }
+});
